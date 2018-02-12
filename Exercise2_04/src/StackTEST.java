9f8a6eb2978011f67daf158b06504937c90e2750
Matej <michalekmato@gmail.com>
2018-02-12 11:51:17 +0100

    Exercise test not finished

---
+++ Exercise2_04/src/StackTEST.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import utility.collection.*;
+
+public class StackTEST {
+
+	@Before
+	public void before() {
+		ArrayStack stack = new ArrayStack();
+	}
+	@Test
+	public void test() {
+		
+		
+	}
+
+}
