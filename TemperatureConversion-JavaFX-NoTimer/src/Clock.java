26321e502e1ac055e4145a920bf587efc96d16f2
Matej <michalekmato@gmail.com>
2018-02-19 08:41:36 +0100

    TempJavaFx

---
+++ TemperatureConversion-JavaFX-NoTimer/src/Clock.java
@@ -0,0 +1,20 @@
+
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class Clock
+{
+   private DateTimeFormatter timeFormatter;
+
+   public Clock()
+   {
+      timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+   }
+
+   public String getCurrentTime()
+   {
+      return LocalTime.now().format(timeFormatter);
+   }
+
+}
---
+++ TemperatureConversion-JavaFX-NoTimer/src/Main.java
@@ -0,0 +1,12 @@
+public class Main
+{
+   public static void main(String args[])
+   {
+      Temperature model = new Temperature();
+      Clock clock = new Clock();
+      TemperatureView view = new TemperatureView();
+      view.startView(model, clock);
+
+      System.out.println("MAIN ENDED");
+   }
+}
